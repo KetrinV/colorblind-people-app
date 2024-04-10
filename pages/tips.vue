6f7a2f3321fd100cb6de47f182b9a75671b336da
Anni Aavik <aavik05@gmail.com>
2024-04-10 14:59:37 +0300

    Initial commit, finished base for website

---
+++ pages/tips.vue
@@ -0,0 +1,30 @@
+<!--Hei! See on .view fail. Kõik HTML läheb template'i sisse-->
+
+<template>
+    <div>
+        <Head/>
+        <Navi/>
+        <section class="content">
+        <div class="container">
+            
+            <section class="sub-herobanner">
+                <div class="title-bg">
+                    <h1 id="title">Tips for Designers!</h1>
+                </div>
+            </section>
+            <section>
+                 <div class="intro">
+                     <h2>Have you ever wondered how to make your website more accessible and User-Friendly for people with color blindness? Here are some tips on what to do and what to avoid!</h2>
+                     <h3>What to do:</h3>
+                     <li>1. Use contrast: Ensure that the text and background have sufficient contrast to aid readability. This benefits not only color-blind individuals but enhances readability for all visitors</li>
+                 </div>
+            </section>
+            <section class="to-do" ></section>
+            <section class="to-avoid" ></section>
+
+        </div>
+        </section>
+
+        <Foot/>
+    </div>
+</template>
