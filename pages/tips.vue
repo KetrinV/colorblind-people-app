838e7baa4853373dd47aadb28d852692a12d54e8
KetrinV <ketrnvanatoa@gmail.com>
2024-04-17 15:48:13 +0300

    adding bulletpoints

--- pages/tips.vue
+++ pages/tips.vue
@@ -12,9 +12,18 @@
             </section>
             <section>
                  <div class="intro">
-                     <h2>Have you ever wondered how to make your website more accessible and User-Friendly for people with color blindness? Here are some tips on what to do and what to avoid!</h2>
-                     <h3>What to do:</h3>
-                     <li>1. Use contrast: Ensure that the text and background have sufficient contrast to aid readability. This benefits not only color-blind individuals but enhances readability for all visitors</li>
+                     <h2 class="header-text">Have you <mark>ever</mark> wondered how to make your website more accessible and User-Friendly for people with <mark>color blindness</mark> ? </h2>
+                     <h2 class="header-text">Here are some tips on what to do and what to avoid!</h2>
+                     <h3 class="subtitle">What to do:</h3>
+
+                     <ol>
+                        <li class="bullet-text"><b id="bullet"><img src="" alt="">1.Use contrast:</b> Ensure that the text and background have sufficient contrast to aid readability. This benefits not only color-blind individuals but enhances readability for all visitors</li>
+                        <li class="bullet-text"><b id="bullet"><img src="" alt="">2. Alternative text for images:</b> Provide alternative texts for all images, describing the content of the image. This is crucial as color-blind individuals may not grasp the message based on color alone.</li>
+                        <li class="bullet-text"><b id="bullet"><img src="" alt="">3. Clear and simple design:</b> Maintain a clear and straightforward design, avoiding excessive color or complex layouts. This simplifies navigation and comprehension for all users.</li>
+                        <li class="bullet-text"><b id="bullet"><img src="" alt="">4. Diversity in color communication:</b> Don't rely solely on color to convey messages. Use other visual elements such as icons and patterns to impart crucial information.</li>
+                        <li class="bullet-text"><b id="bullet"><img src="" alt="">5. Check color schemes:</b> Test different color schemes to ensure your website is understandable and readable for various types of color vision deficiency. Utilize tools that simulate different vision impairments.</li>
+                    </ol>
+                     
                  </div>
             </section>
             <section class="to-do" ></section>
