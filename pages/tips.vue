ba641fe8ae195f7c27e0fc73ff4866bd07c5032c
KetrinV <ketrnvanatoa@gmail.com>
2024-04-19 09:35:11 +0300

    added style root

--- pages/tips.vue
+++ pages/tips.vue
@@ -16,14 +16,14 @@
                      <h2 class="header-text">Here are some tips on what to do and what to avoid!</h2>
                      <h3 class="subtitle">What to do:</h3>
 
-                     <ol>
+                     <ol class="bullet-container">
                         <li class="bullet-text"><b id="bullet"><img class="tips-icons" src="/tips-icons/stars.png" alt="">1.Use contrast:</b> Ensure that the text and background have sufficient contrast to aid readability. This benefits not only color-blind individuals but enhances readability for all visitors</li>
                         <li class="bullet-text"><b id="bullet"><img class="tips-icons" src="/tips-icons/text-size.png" alt="">2. Alternative text for images:</b> Provide alternative texts for all images, describing the content of the image. This is crucial as color-blind individuals may not grasp the message based on color alone.</li>
                         <li class="bullet-text"><b id="bullet"><img class="tips-icons" src="/tips-icons/lightbulb.png" alt="">3. Clear and simple design:</b> Maintain a clear and straightforward design, avoiding excessive color or complex layouts. This simplifies navigation and comprehension for all users.</li>
                         <li class="bullet-text"><b id="bullet"><img class="tips-icons" src="/tips-icons/shapes.png" alt="">4. Diversity in color communication:</b> Don't rely solely on color to convey messages. Use other visual elements such as icons and patterns to impart crucial information.</li>
                         <li class="bullet-text"><b id="bullet"><img class="tips-icons" src="/tips-icons/swatchbook.png" alt="">5. Check color schemes:</b> Test different color schemes to ensure your website is understandable and readable for various types of color vision deficiency. Utilize tools that simulate different vision impairments.</li>
                     </ol>
-                     
+                     <hr>
                  </div>
             </section>
             <section class="to-do" ></section>
