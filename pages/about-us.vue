284f628c71d929357f2a4fdeba34ade14abeedd9
KetrinV <ketrnvanatoa@gmail.com>
2024-04-22 02:05:03 +0300

    about-us styling

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -5,15 +5,17 @@
         <Head/>
         <Navi/>
         <section class="content">
-        <div class="container">
+        <div class="about-container">
             <div class="title-bg">
                 <h1 id="title">About <mark>us!</mark></h1> 
             </div> 
-            <div class="intro">
-                <p>Welcome to <mark>ColorMaze</mark>, a platform dedicated to providing comprehensive information on colors, color perception, and color blindness. </p>
-                <p>Our mission is to empower designers and developers with a range of tools to test and enhance the accessibility of their web solutions. Whether you're a seasoned designer or a developer seeking to improve user experience, we offer insight and practical tips to make your web solutions more inclusive.</p>
-                <p>Join us on a journey to create a web environment where everyone, regardless of their ability to perceive colors, can engage seamlessly with digital content. Explore our tools and tips to transform your web solutions into accessible and user-friendly experiences.</p> 
-        </div>
+            <div class="intro-about">
+                <p class="about-text">Welcome to <mark>ColorMaze</mark>, a platform dedicated to providing comprehensive information on colors, color perception, and color blindness. </p>
+                <br>
+                <p class="about-text">Our mission is to empower designers and developers with a range of tools to test and enhance the accessibility of their web solutions. Whether you're a seasoned designer or a developer seeking to improve user experience, we offer insight and practical tips to make your web solutions more inclusive.</p>
+                <br>
+                <p class="about-text">Join us on a journey to create a web environment where everyone, regardless of their ability to perceive colors, can engage seamlessly with digital content. Explore our tools and tips to transform your web solutions into accessible and user-friendly experiences.</p> 
+            </div>
         </div>
         </section>
 
