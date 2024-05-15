6940c51bac7312e2948fc8b1b54cdb2610b6b621
KetrinV <ketrnvanatoa@gmail.com>
2024-05-15 09:10:36 +0300

    footer link

--- components/Foot.vue
+++ components/Foot.vue
@@ -2,7 +2,9 @@
         <footer class="footer">
     <p>&copy; ColorMaze 2024</p>
     <ul>
-        <li><a href="#">About</a></li>
+        <li>
+            <nuxt-link to="../about-us">About</nuxt-link>
+        </li>
         <li><a href="#">Contact</a></li>
         <li><a href="#">Terms of Service</a></li>
         <li><a href="#">Privacy Policy</a></li>
